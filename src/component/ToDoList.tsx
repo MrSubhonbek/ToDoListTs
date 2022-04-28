a3647a7b86dacb5e86382ef23f1c84bb24a80e56
Subhonbek <basubhonbek@gmail.com>
2022-04-28 10:12:25 +0300

    ToDoList

---
+++ src/component/ToDoList.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+interface ITasks{
+    id: number,
+    title: string,
+    isDone: boolean
+};
+
+interface IProps {
+    titleName: string,
+    tasks: Array<ITasks>
+};
+
+function ToDolist(props: IProps) {
+    return (
+        <div>
+            <h3>{props.titleName}</h3>
+            <div>
+                <input />
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={true} /> <span>{props.tasks[0].title}</span></li>
+                <li><input type="checkbox" checked={true} /> <span>{props.tasks[1].title}</span></li>
+                <li><input type="checkbox" checked={false} /> <span>{props.tasks[2].title}</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+}
+export default ToDolist;
